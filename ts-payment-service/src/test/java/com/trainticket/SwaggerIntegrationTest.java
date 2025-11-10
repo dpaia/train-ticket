a6a5015ded386f33984140c065dcae02a7ca2be7
jf20250701 <jf20250701@gmail.com>
2025-11-10 04:37:56 -0800

    Payments and Assurance: Document Payment Service APIs with OpenAPI/Swagger Fixed #36

--- ts-payment-service/src/test/java/com/trainticket/SwaggerIntegrationTest.java
+++ ts-payment-service/src/test/java/com/trainticket/SwaggerIntegrationTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.lang.reflect.Method;
@@ -25,6 +26,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @RunWith(SpringRunner.class)
+@ActiveProfiles("test")
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
